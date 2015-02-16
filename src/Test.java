9599cb2181f12bbbdaa8570b7f245abffac14885
Sindhu <sindhu.pothireddy@gmail.com>
2015-02-16 09:53:55 -0800

    Updated averages for priority

--- src/Test.java
+++ src/Test.java
@@ -10,6 +10,26 @@ public class Test {
 	private static float tempAverageResponseTime =0;
 	private static float tempAverageThroughputTime = 0;
 	
+	private static float tempP1AverageWaitingTime = 0;
+	private static float tempP1AverageTurnaroundTime = 0;
+	private static float tempP1AverageResponseTime =0;
+	private static float tempP1AverageThroughputTime = 0;
+	
+	private static float tempP2AverageWaitingTime = 0;
+	private static float tempP2AverageTurnaroundTime = 0;
+	private static float tempP2AverageResponseTime =0;
+	private static float tempP2AverageThroughputTime = 0;
+	
+	private static float tempP3AverageWaitingTime = 0;
+	private static float tempP3AverageTurnaroundTime = 0;
+	private static float tempP3AverageResponseTime =0;
+	private static float tempP3AverageThroughputTime = 0;
+	
+	private static float tempP4AverageWaitingTime = 0;
+	private static float tempP4AverageTurnaroundTime = 0;
+	private static float tempP4AverageResponseTime =0;
+	private static float tempP4AverageThroughputTime = 0;
+	
 	public static void main(String[] args) {
 
 		System.out.println("FIRST-COME FIRST-SERVED: ");
@@ -18,7 +38,8 @@ public class Test {
 			FCFS fcfs = new FCFS(createProcessList());
 			runAlgorithm(fcfs);
 		}
-		printAveragesForAlgorithm();
+		System.out.println("FIRST-COME FIRST-SERVED: ");
+		printAveragesForAlgorithm(false);
 		
 		System.out.println("SHORTEST JOB FIRST: ");
 		for (int i = 1; i <= 5; i++) {
@@ -26,7 +47,8 @@ public class Test {
 			SJF sjf = new SJF(createProcessList());
 			runAlgorithm(sjf);
 		}
-		printAveragesForAlgorithm();
+		System.out.println("SHORTEST JOB FIRST: ");
+		printAveragesForAlgorithm(false);
 		
 		System.out.println("SHORTEST REMAINING TIME: ");
 		for (int i = 1; i <= 5; i++) {
@@ -34,7 +56,8 @@ public class Test {
 			SRT srt = new SRT(createProcessList());
 			runAlgorithm(srt);
 		}
-		printAveragesForAlgorithm();
+		System.out.println("SHORTEST REMAINING TIME: ");
+		printAveragesForAlgorithm(false);
 
 		System.out.println("ROUND ROBIN: ");
 		for (int i = 1; i <= 5; i++) {
@@ -42,7 +65,8 @@ public class Test {
 			RR rr = new RR(createProcessList());
 			runAlgorithm(rr);
 		}
-		printAveragesForAlgorithm();
+		System.out.println("ROUND ROBIN: ");
+		printAveragesForAlgorithm(false);
 		
 		System.out.println("HPF Preemptive: ");
 		for (int i = 1; i <= 5; i++) {
@@ -56,11 +80,12 @@ public class Test {
 			System.out.println();
 			calculateAveragesOnPriority(hpfp.getFinishedList());
 			System.out.println("Overall: ");
-			calculateAverageTimes(hpfp.getFinishedList());
+			calculateAverageTimes(hpfp.getFinishedList(), 0);
 			System.out.println("**********************************************");
 			
 		}
-		printAveragesForAlgorithm();
+		System.out.println("HPF Preemptive: ");
+		printAveragesForAlgorithm(true);
 		
 		
 		System.out.println("HPF Nonpreemptive: ");
@@ -75,27 +100,50 @@ public class Test {
 			System.out.println();
 			calculateAveragesOnPriority(hpfnp.getFinishedList());
 			System.out.println("Overall: ");
-			calculateAverageTimes(hpfnp.getFinishedList());
+			calculateAverageTimes(hpfnp.getFinishedList(), 0);
 			System.out.println("**********************************************");
 			
 		}
-		printAveragesForAlgorithm();
+		System.out.println("HPF Nonpreemptive: ");
+		printAveragesForAlgorithm(true);
 		
-		System.out.println("HPFNonpreemptiveAging: ");
+		System.out.println("EXTRA CREDIT!");
+		System.out.println("HPF Nonpreemptive Aging: ");
 		for (int i = 1; i <= 5; i++) {
-			System.out.println("HPFNonpreemptiveAging Run #" + i + ": ");
+			System.out.println("HPF Nonpreemptive Aging Run #" + i + ": ");
 			HPFNonpreemptiveAging hpfnpA = new HPFNonpreemptiveAging(createProcessList());
-			runAlgorithm(hpfnpA);
+			for (Process p : hpfnpA.returnArrival()) {
+				p.display();
+			}
+			System.out.println("Timeline: ");
+			hpfnpA.runAlgorithm();
+			System.out.println();
+			calculateAveragesOnPriority(hpfnpA.getFinishedList());
+			System.out.println("Overall: ");
+			calculateAverageTimes(hpfnpA.getFinishedList(), 0);
+			System.out.println("**********************************************");
 			
 		}
+		System.out.println("HPF Nonpreemptive Aging: ");
+		printAveragesForAlgorithm(true);
 		
 		System.out.println("HPF Preemptive Aging: ");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println("HPF Preemptive Aging Run #" + i + ": ");
 			HPFPreemptiveAging hpfpA = new HPFPreemptiveAging(createProcessList());
-			runAlgorithm(hpfpA);
+			for (Process p : hpfpA.returnArrival()) {
+				p.display();
+			}
+			System.out.println("Timeline: ");
+			hpfpA.runAlgorithm();
+			System.out.println();
+			calculateAveragesOnPriority(hpfpA.getFinishedList());
+			System.out.println("Overall: ");
+			calculateAverageTimes(hpfpA.getFinishedList(), 0);
+			System.out.println("**********************************************");
 		}
-		printAveragesForAlgorithm();
+		System.out.println("HPF Preemptive Aging: ");
+		printAveragesForAlgorithm(true);
 
 	}
 
@@ -129,7 +177,7 @@ public class Test {
 	 * 
 	 * @param finishedProcesses
 	 */
-	private static void calculateAverageTimes(List<Process> finishedProcesses) {
+	private static void calculateAverageTimes(List<Process> finishedProcesses, int priorityLevel) {
 		float waitingTime = 0;
 		float turnaroundTime = 0; // the time in ready list to completed execute
 		float responseTime = 0;
@@ -145,15 +193,59 @@ public class Test {
 		waitingTime = waitingTime / finishedProcesses.size();
 		turnaroundTime = turnaroundTime / finishedProcesses.size();
 		responseTime = responseTime / finishedProcesses.size();
+		switch(priorityLevel){
+		case 0: //not based on priority level
+			tempAverageWaitingTime += waitingTime;
+			tempAverageTurnaroundTime += turnaroundTime;
+			tempAverageResponseTime += responseTime;
+	
+			System.out.println("Average Waiting Time: " + waitingTime);
+			System.out.println("Average Turnaround Time: " + turnaroundTime);
+			System.out.println("Average Response Time: " + responseTime);
+			System.out.println("Throughput: "+ throughput);
+			break;
+		case 1:
+			tempP1AverageWaitingTime += waitingTime;
+			tempP1AverageTurnaroundTime += turnaroundTime;
+			tempP1AverageResponseTime += responseTime;
+	
+			System.out.println("Average Waiting Time: " + waitingTime);
+			System.out.println("Average Turnaround Time: " + turnaroundTime);
+			System.out.println("Average Response Time: " + responseTime);
+			System.out.println("Throughput: "+ throughput);
+			break;
+		case 2:
+			tempP2AverageWaitingTime += waitingTime;
+			tempP2AverageTurnaroundTime += turnaroundTime;
+			tempP2AverageResponseTime += responseTime;
+	
+			System.out.println("Average Waiting Time: " + waitingTime);
+			System.out.println("Average Turnaround Time: " + turnaroundTime);
+			System.out.println("Average Response Time: " + responseTime);
+			System.out.println("Throughput: "+ throughput);
+			break;
+		case 3: 
+			tempP3AverageWaitingTime += waitingTime;
+			tempP3AverageTurnaroundTime += turnaroundTime;
+			tempP3AverageResponseTime += responseTime;
+	
+			System.out.println("Average Waiting Time: " + waitingTime);
+			System.out.println("Average Turnaround Time: " + turnaroundTime);
+			System.out.println("Average Response Time: " + responseTime);
+			System.out.println("Throughput: "+ throughput);
+			break;
+		case 4:
+			tempP4AverageWaitingTime += waitingTime;
+			tempP4AverageTurnaroundTime += turnaroundTime;
+			tempP4AverageResponseTime += responseTime;
+	
+			System.out.println("Average Waiting Time: " + waitingTime);
+			System.out.println("Average Turnaround Time: " + turnaroundTime);
+			System.out.println("Average Response Time: " + responseTime);
+			System.out.println("Throughput: "+ throughput);
+			break;
+		}
 		
-		tempAverageWaitingTime += waitingTime;
-		tempAverageTurnaroundTime += turnaroundTime;
-		tempAverageResponseTime += responseTime;
-
-		System.out.println("Average Waiting Time: " + waitingTime);
-		System.out.println("Average Turnaround Time: " + turnaroundTime);
-		System.out.println("Average Response Time: " + responseTime);
-		System.out.println("Throughput: "+ throughput);
 
 	}
 	
@@ -185,16 +277,16 @@ public class Test {
 		}
 		
 		System.out.println("Priority 1: ");
-		calculateAverageTimes(priority1);
+		calculateAverageTimes(priority1, 1);
 		
 		System.out.println("Priority 2: ");
-		calculateAverageTimes(priority2);
+		calculateAverageTimes(priority2, 2);
 		
 		System.out.println("Priority 3: ");
-		calculateAverageTimes(priority3);
+		calculateAverageTimes(priority3, 3);
 		
 		System.out.println("Priority 4: ");
-		calculateAverageTimes(priority4);
+		calculateAverageTimes(priority4, 4);
 		
 		
 		
@@ -213,7 +305,7 @@ public class Test {
 		System.out.println("Timeline: ");
 		algo.runAlgorithm();
 		System.out.println();
-		calculateAverageTimes(algo.getFinishedList());
+		calculateAverageTimes(algo.getFinishedList(), 0);
 		System.out.println("**********************************************");
 
 	}
@@ -226,20 +318,71 @@ public class Test {
 		tempAverageTurnaroundTime = 0;
 		tempAverageResponseTime =0;
 		tempAverageThroughputTime = 0;
+		
+		tempP1AverageWaitingTime = 0;
+		tempP1AverageTurnaroundTime = 0;
+		tempP1AverageResponseTime =0;
+		tempP1AverageThroughputTime = 0;
+		
+		tempP2AverageWaitingTime = 0;
+		tempP2AverageTurnaroundTime = 0;
+		tempP2AverageResponseTime =0;
+		tempP2AverageThroughputTime = 0;
+		
+		tempP3AverageWaitingTime = 0;
+		tempP3AverageTurnaroundTime = 0;
+		tempP3AverageResponseTime =0;
+		tempP3AverageThroughputTime = 0;
+		
+		tempP3AverageWaitingTime = 0;
+		tempP3AverageTurnaroundTime = 0;
+		tempP3AverageResponseTime =0;
+		tempP3AverageThroughputTime = 0;
 	}
 	
 	/**
 	 * Print out averages for 5 runs of the algorithm
 	 */
-	public static void printAveragesForAlgorithm(){
+	public static void printAveragesForAlgorithm(boolean containsPriority){
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		System.out.println("Averages for the 5 runs: ");
+		if(containsPriority){
+			System.out.println("Priority 1: ");
+			System.out.println("Average Waiting Time: " + (tempP1AverageWaitingTime/5));
+			System.out.println("Average Turnaround Time: " + (tempP1AverageTurnaroundTime/5));
+			System.out.println("Average Response Time: " + (tempP1AverageResponseTime/5));
+			
+			System.out.println("Priority 2: ");
+			System.out.println("Average Waiting Time: " + (tempP2AverageWaitingTime/5));
+			System.out.println("Average Turnaround Time: " + (tempP2AverageTurnaroundTime/5));
+			System.out.println("Average Response Time: " + (tempP2AverageResponseTime/5));
+			
+			System.out.println("Priority 3: ");
+			System.out.println("Average Waiting Time: " + (tempP3AverageWaitingTime/5));
+			System.out.println("Average Turnaround Time: " + (tempP3AverageTurnaroundTime/5));
+			System.out.println("Average Response Time: " + (tempP3AverageResponseTime/5));
+			
+			System.out.println("Priority 4: ");
+			System.out.println("Average Waiting Time: " + (tempP4AverageWaitingTime/5));
+			System.out.println("Average Turnaround Time: " + (tempP4AverageTurnaroundTime/5));
+			System.out.println("Average Response Time: " + (tempP4AverageResponseTime/5));
+		}
+		
+		
+		System.out.println("Overall: ");
 		System.out.println("Average Waiting Time: " + (tempAverageWaitingTime/5));
 		System.out.println("Average Turnaround Time: " + (tempAverageTurnaroundTime/5));
 		System.out.println("Average Response Time: " + (tempAverageResponseTime/5));
+		
+		
+		
+		
+		
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		
 		resetTempAverages(); //reset values to zero;
 	}
+	
+
 
 }
